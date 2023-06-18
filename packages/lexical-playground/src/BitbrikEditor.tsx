6d5037d46ef4500cf4a11ede621330d7cd5acbc2
Tâm Vũ Minh <tam.vm@teko.vn>
2023-06-18 10:39:46 +0700

    create a handle to export html

--- packages/lexical-playground/src/BitbrikEditor.tsx
+++ packages/lexical-playground/src/BitbrikEditor.tsx
@@ -8,9 +8,7 @@
 import './index.css';
 
 import {$insertGeneratedNodes} from '@lexical/clipboard';
-import {$generateNodesFromDOM} from '@lexical/html';
-import {$createLinkNode} from '@lexical/link';
-import {$createListItemNode, $createListNode} from '@lexical/list';
+import {$generateHtmlFromNodes,$generateNodesFromDOM} from '@lexical/html';
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {CheckListPlugin} from '@lexical/react/LexicalCheckListPlugin';
 import {ClearEditorPlugin} from '@lexical/react/LexicalClearEditorPlugin';
@@ -26,15 +24,9 @@ import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {TabIndentationPlugin} from '@lexical/react/LexicalTabIndentationPlugin';
 import {TablePlugin} from '@lexical/react/LexicalTablePlugin';
 import useLexicalEditable from '@lexical/react/useLexicalEditable';
-import {$createHeadingNode, $createQuoteNode} from '@lexical/rich-text';
-import {
-  $createParagraphNode,
-  $createRangeSelection,
-  $createTextNode,
-  $getRoot,
-} from 'lexical';
+import {$createRangeSelection} from 'lexical';
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {forwardRef, useEffect, useImperativeHandle,useState} from 'react';
 import {CAN_USE_DOM} from 'shared/canUseDOM';
 
 import {SettingsContext, useSettings} from './context/SettingsContext';
@@ -205,91 +197,14 @@ function Editor(): JSX.Element {
   );
 }
 
-function prepopulatedRichText() {
-  const root = $getRoot();
-  if (root.getFirstChild() === null) {
-    const heading = $createHeadingNode('h1');
-    heading.append($createTextNode('Welcome to the playground'));
-    root.append(heading);
-    const quote = $createQuoteNode();
-    quote.append(
-      $createTextNode(
-        `In case you were wondering what the black box at the bottom is – it's the debug view, showing the current state of the editor. ` +
-          `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`,
-      ),
-    );
-    root.append(quote);
-    const paragraph = $createParagraphNode();
-    paragraph.append(
-      $createTextNode('The playground is a demo environment built with '),
-      $createTextNode('@lexical/react').toggleFormat('code'),
-      $createTextNode('.'),
-      $createTextNode(' Try typing in '),
-      $createTextNode('some text').toggleFormat('bold'),
-      $createTextNode(' with '),
-      $createTextNode('different').toggleFormat('italic'),
-      $createTextNode(' formats.'),
-    );
-    root.append(paragraph);
-    const paragraph2 = $createParagraphNode();
-    paragraph2.append(
-      $createTextNode(
-        'Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!',
-      ),
-    );
-    root.append(paragraph2);
-    const paragraph3 = $createParagraphNode();
-    paragraph3.append(
-      $createTextNode(`If you'd like to find out more about Lexical, you can:`),
-    );
-    root.append(paragraph3);
-    const list = $createListNode('bullet');
-    list.append(
-      $createListItemNode().append(
-        $createTextNode(`Visit the `),
-        $createLinkNode('https://lexical.dev/').append(
-          $createTextNode('Lexical website'),
-        ),
-        $createTextNode(` for documentation and more information.`),
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Check out the code on our `),
-        $createLinkNode('https://github.com/facebook/lexical').append(
-          $createTextNode('GitHub repository'),
-        ),
-        $createTextNode(`.`),
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Playground code can be found `),
-        $createLinkNode(
-          'https://github.com/facebook/lexical/tree/main/packages/lexical-playground',
-        ).append($createTextNode('here')),
-        $createTextNode(`.`),
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Join our `),
-        $createLinkNode('https://discord.com/invite/KmG4wQnnD9').append(
-          $createTextNode('Discord Server'),
-        ),
-        $createTextNode(` and chat with the team.`),
-      ),
-    );
-    root.append(list);
-    const paragraph4 = $createParagraphNode();
-    paragraph4.append(
-      $createTextNode(
-        `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`,
-      ),
-    );
-    root.append(paragraph4);
-  }
-}
-
 type BitbrikEditorProps = {
   initialHtml: string;
 };
 
-function App({initialHtml}: BitbrikEditorProps): JSX.Element {
+const App = forwardRef(function (
+  {initialHtml}: BitbrikEditorProps,
+  ref,
+): JSX.Element {
   const [editor] = useLexicalComposerContext();
   useEffect(() => {
     if (initialHtml) {
@@ -307,6 +222,19 @@ function App({initialHtml}: BitbrikEditorProps): JSX.Element {
     }
   }, [initialHtml, editor]);
 
+  useImperativeHandle(ref, () => {
+    return {
+      exportHtml: () => {
+        return new Promise((resolve) => {
+          editor.update(() => {
+            const htmlContent = $generateHtmlFromNodes(editor);
+            resolve(htmlContent);
+          });
+        });
+      },
+    };
+  });
+
   return (
     <SharedHistoryContext>
       <TableContext>
@@ -318,11 +246,13 @@ function App({initialHtml}: BitbrikEditorProps): JSX.Element {
       </TableContext>
     </SharedHistoryContext>
   );
-}
+});
 
-export function BitbrikEditor({initialHtml}: BitbrikEditorProps): JSX.Element {
+export const BitbrikEditor = forwardRef(function (
+  {initialHtml}: BitbrikEditorProps,
+  ref,
+): JSX.Element {
   const initialConfig = {
-    editorState: prepopulatedRichText,
     namespace: 'Playground',
     nodes: [...PlaygroundNodes],
     onError: (error: Error) => {
@@ -334,8 +264,8 @@ export function BitbrikEditor({initialHtml}: BitbrikEditorProps): JSX.Element {
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <SettingsContext>
-        <App initialHtml={initialHtml} />
+        <App initialHtml={initialHtml} ref={ref} />
       </SettingsContext>
     </LexicalComposer>
   );
-}
+});
